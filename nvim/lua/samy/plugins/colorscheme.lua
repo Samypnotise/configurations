19743683c3241e51fd736d7323573bb51c82bcdf
Samypnotise <SamyDjemiliSD@gmail.com>
2024-10-26 21:34:39 +0200

    feat: flutter support & noice

--- nvim/lua/samy/plugins/colorscheme.lua
+++ nvim/lua/samy/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    require("tokyonight").setup({
-      style = "storm",
-    })
-    vim.cmd("colorscheme tokyonight")
-  end
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			style = "storm",
+			on_colors = function(colors) end,
+			on_highlights = function(highlights, colors) end,
+		})
+		vim.cmd("colorscheme tokyonight")
+	end,
 }
