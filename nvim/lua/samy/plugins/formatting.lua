2ad7108632063df37951069de8614de6de6255bd
Samypnotise <SamyDjemiliSD@gmail.com>
2024-10-26 22:34:21 +0200

    style: formatting

--- nvim/lua/samy/plugins/formatting.lua
+++ nvim/lua/samy/plugins/formatting.lua
@@ -28,7 +28,7 @@ return {
 			},
 		})
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		vim.keymap.set({ "n", "v" }, "<leader>fmt", function()
 			conform.format({
 				lsp_fallback = true,
 				async = false,
