f6c635ea21c822f39fd96ac79b2e3fc5e3afa8b2
Samypnotise <SamyDjemiliSD@gmail.com>
2024-11-25 14:12:38 +0100

    chore: setting up new color scheme

--- nvim/lua/samy/plugins/colorscheme.lua
+++ nvim/lua/samy/plugins/colorscheme.lua
@@ -1,12 +1,40 @@
+-- Tokyonight theme
+-- return {
+-- 	"folke/tokyonight.nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("tokyonight").setup({
+-- 			style = "storm",
+-- 			on_colors = function(colors) end,
+-- 			on_highlights = function(highlights, colors) end,
+-- 		})
+-- 		vim.cmd("colorscheme tokyonight")
+-- 	end,
+-- }
+
+-- Kanagawa theme
+-- return {
+-- 	"rebelot/kanagawa.nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("kanagawa").setup({})
+-- 		vim.cmd("colorscheme kanagawa-wave")
+-- 	end,
+-- }
+
 return {
-	"folke/tokyonight.nvim",
+	"catppuccin/nvim",
 	priority = 1000,
 	config = function()
-		require("tokyonight").setup({
-			style = "storm",
-			on_colors = function(colors) end,
-			on_highlights = function(highlights, colors) end,
+		require("catppuccin").setup({
+			color_overrides = {
+				macchiato = {
+					pink = "#9D9DFF",
+					red = "#F96F6F",
+					teal = "#EED59E",
+				},
+			},
 		})
-		vim.cmd("colorscheme tokyonight")
+		vim.cmd("colorscheme catppuccin-macchiato")
 	end,
 }
