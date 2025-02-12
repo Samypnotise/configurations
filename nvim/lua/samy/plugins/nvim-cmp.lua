9ffc6a2a14d1beeacc23f4ec839266255704d50d
Samypnotise <samy.djemili.dev@infyneis.com>
2025-02-12 14:53:27 +0100

    feat(nvim-cmp): adding emojis

--- nvim/lua/samy/plugins/nvim-cmp.lua
+++ nvim/lua/samy/plugins/nvim-cmp.lua
@@ -2,8 +2,9 @@ return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
+		"hrsh7th/cmp-buffer", -- source pour compléter le texte déjà présent dans le buffer
+		"hrsh7th/cmp-path", -- source pour compléter les chemins des fichiers
+		"hrsh7th/cmp-cmdline", -- source pour les completions de la cmdline de vim
 		{
 			"L3MON4D3/LuaSnip",
 			-- follow latest release.
@@ -13,6 +14,7 @@ return {
 		},
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
+		"hrsh7th/cmp-emoji", -- complétion d'émojis à la saisie de :
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
 	config = function()
@@ -21,12 +23,6 @@ return {
 		local luasnip = require("luasnip")
 
 		local lspkind = require("lspkind")
-
-		local has_words_before = function()
-			unpack = unpack or table.unpack
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
 
 		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
@@ -47,7 +43,7 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-s"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
@@ -55,15 +51,50 @@ return {
 				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
+				{ name = "emoji" }, -- emojis
 			}),
 
 			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
+				-- Comportement par défaut
+				expandable_indicator = true,
+				-- Champs affichés par défaut
+				fields = { "abbr", "kind", "menu" },
 				format = lspkind.cmp_format({
-					maxwidth = 50,
-					ellipsis_char = "...",
+					mode = "symbol_text",
+					-- On suffixe chaque entrée par son type
+					menu = {
+						buffer = "[Buffer]",
+						luasnip = "[LuaSnip]",
+						nvim_lua = "[Lua]",
+						path = "[Path]",
+						emoji = "[Emoji]",
+					},
 				}),
 			},
+		})
+
+		-- `/` complétion
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
+
+		-- `:` complétion
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
+			}),
 		})
 	end,
 }
